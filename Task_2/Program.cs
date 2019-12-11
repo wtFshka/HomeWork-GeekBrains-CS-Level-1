502510a855c555c91e1e92a031f555e4f22b9e07
Sergey Deev <sdeev5@mvd.ru>
2019-12-11 17:49:40 +0300

    Add Task 1 and Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = new List<double>();
+            double num, sum = 0;
+            Console.WriteLine("Программа посчета суммы всех введенных нечетных положительных чисел, для вывода результата и выхода из программы введите 0");
+            do
+            {
+                if (double.TryParse(Console.ReadLine().Replace(',','.'), out num))
+                {
+                    if (num % 2 == 1 && num > 0)
+                    {
+                        numbers.Add(num); 
+                    }
+                } else
+                {
+                    Console.WriteLine("Что-то не похоже на число... Попробуйте ещё раз или ввидите 0");
+                    num = -1;
+                }    
+            } while (num != 0);
+            Console.WriteLine($"Вами было введено {numbers.Count} нечетных положительных чисел(а):");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+                sum += n;
+            }
+            Console.WriteLine($"Их сумма ровна: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
