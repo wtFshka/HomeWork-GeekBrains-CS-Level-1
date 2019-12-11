61ed2409a95bd3fc87301ebf92e712ee08ee293b
Sergey Deev <ilamaboti@gmail.com>
2019-12-11 22:28:42 +0300

    Add Lesson 1

---
+++ MyClass/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyMethods
+{
+    public class WriterReader
+    {
+        public static void Main()
+        {
+
+        }
+        public static string WriteRead(string text)
+        {
+            Console.WriteLine(text);
+            return Console.ReadLine();
+        }
+        public static void Print(string text, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(text);
+        }
+        public static void Pause(string text)
+        {
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
