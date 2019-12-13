ca165f0b3fd52f7fac3bee0380e3c1e80778e50f
Sergey Deev <ilamaboti@gmail.com>
2019-12-13 19:38:38 +0300

    Add Tasks 1,2,3

---
+++ Lesson 4/Task_1/StaticClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Task_1_2
+{
+    public static class StaticClass
+    {
+        public static int[] GetArrayFromFile(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                return Array.ConvertAll(File.ReadAllLines(filename), x => Convert.ToInt32(x));
+            }
+            else
+            {
+                return CreatRandomArray();
+            }
+        }
+
+        public static int[] CreatRandomArray(int lenghtArray = 20)
+        {
+            int[] array = new int[lenghtArray];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-10000, 10001);
+            }
+            return array;
+        }
+        public static void CreatRandomArray(string filename, int lenghtArray = 20)
+        {
+            int[] array = new int[lenghtArray];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-10000, 10001);
+            }
+            File.WriteAllLines(filename, Array.ConvertAll(array, x => x.ToString()));
+        }
+        public static void FindPairs(int[] array)
+        {
+            int count = 0;
+            if (array.Length > 1)
+            {
+                Console.Write($"Весь массив: ");
+                foreach (int a in array)
+                {
+                    Console.Write($"{a}; ");
+                }
+                Console.WriteLine();
+                for (int i = 0; i < array.Length-1; i++)
+                {
+                    if (array[i]%3==0 ^ array[i+1]%3==0)
+                    {
+                        Console.WriteLine($"{array[i]} и {array[i+1]}");
+                        count++;
+                    }
+                }
+                Console.WriteLine($"Всего подходящих пар: {count}");
+            }
+        }
+    }
+}
---
+++ Lesson 4/Task_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyArray;
+
+namespace Task_3
+{
+    class Program
+    {
+        //а) Дописать класс для работы с одномерным массивом. 
+        //Реализовать конструктор, создающий массив определенного размера и заполняющий массив числами от начального значения с заданным шагом.
+        //Создать свойство Sum, которое возвращает сумму элементов массива, метод Inverse, возвращающий новый массив с измененными знаками у
+        //всех элементов массива (старый массив, остается без изменений), метод Multi, умножающий каждый элемент массива на определённое число,
+        //свойство MaxCount, возвращающее количество максимальных элементов.
+        //б)** Cоздать библиотеку содержащую класс для работы с массивом.
+        //Продемонстрировать работу библиотеки 
+
+        //е)*** Подсчитать частоту вхождения каждого элемента в массив(коллекция Dictionary<int, int>)
+
+        static void Main()
+        {
+            Console.WriteLine("Программа для работы с массивом");
+            MyArray.MyArray mr = new MyArray.MyArray(5, 3, 2);
+            Console.Write($"Созданный массив: ");
+            Console.WriteLine(mr);
+            Console.WriteLine($"Сумма чисел массива: {mr.Sum}");
+            int[] array = mr.Inverse();
+            string strArr = "";
+            foreach (var a in array)
+            {
+                strArr += $" {a}";
+            }
+            Console.Write($"Созданный массив с измененными знаками: ");
+            Console.WriteLine(strArr);
+            Console.Write($"Первый созданный массив: ");
+            Console.WriteLine(mr);
+            mr.Multi(2);
+            Console.Write($"Первый массив с эелементами перемноженными на 2: ");
+            Console.WriteLine(mr);
+            Console.Write($"Количество максимальных чисел в первом массиве: ");
+            Console.WriteLine(mr.MaxCount);
+            Console.ReadKey();
+        }
+    }
+
+}
